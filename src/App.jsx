a7a0c77e68037ebfb3e34ffc7d1deaf4477e2208
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-10-05 01:00:28 -0400

    Adicionando QRcode

--- src/App.jsx
+++ src/App.jsx
@@ -81,6 +81,16 @@ export default function App() {
           <img className="logoProject" src={imageNotFound} alt="" />
         </div>
       </div>
+      <div className="qrCode">
+        <a
+          href="br.qr-code-generator.com/"
+          border="0"
+          // style="cursor:default"
+          rel="nofollow"
+        >
+          <img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fenick.dev.br%2F&chs=180x180&choe=UTF-8&chld=L|2" />
+        </a>
+      </div>
     </>
   )
 }
