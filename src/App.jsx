b382279bc3fb8142599a1dafc4a831b06455b448
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-10-25 16:36:05 -0400

    Creating `LoadingPage.jsx` with text being typed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import { Footer } from "./components/Footer";
-import { Hero } from "./components/Hero";
-import { Navigation } from "./components/Navigation";
-import { Projects } from "./components/Projects";
+import { useEffect, useState } from 'react'
+import { Footer } from './components/Footer'
+import { Hero } from './components/Hero'
+import { LoadingPage } from './components/LoadingPage'
+import { Navigation } from './components/Navigation'
+import { Projects } from './components/Projects'
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 5000)
+  }, [])
+
+  if (isLoading) {
+    return <LoadingPage />
+  }
+
   return (
     <div className="bg-gray-400 h-screen p-4">
       <Navigation />
