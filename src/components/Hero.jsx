0a3a31cc1a637c97db77f27d5f5b804c8951ca6a
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-10-26 13:44:14 -0400

    modifying text and image of the `Hero` component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,21 @@
+import heroImg from '../assets/heroImg.svg'
+
 export function Hero() {
   return (
-    <div>
-      <h1 className=" text-3xl font-bold text-gray-800 mt-20 md:text-5 md">
-          Oi, sou {'<'}EnickJhony{'/>'} !
+    <div className="flex flex-col items-center py-20 lg:py-8 md:flex-row lg:max-w-5xl m-auto">
+      <div className="flex flex-col items-center">
+        <h1 className="py-1 lg:py-4 text-3xl sm:text-4xl lg:text-5xl font-bold">
+          Oi, sou {'<'}EnickJhony {'/>'}!
         </h1>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/enickjhony"
-        >
-          <img
-            className="rounded-full w-36"
-            src="https://github.com/enickjhony.png"
-            alt="Foto do Enick"
-          />
-        </a>
+        <div className="text-xl sm:text-2xl sm:w-4/5 lg:text-3xl lg:w-full text-slate-400 py-1 lg:py-4 text-center">
+          <p>
+            Front-end Developer, seja bem vindo(a) ao meu pedacinho de
+            internet!
+          </p>
+          <p>Sou apaixonado por interfaces responsivas e funcionais.</p>
+        </div>
+      </div>
+      <img className="w-full sm:w-3/4 md:w-2/5" src={heroImg} alt="" />
     </div>
   )
 }
