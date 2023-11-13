a9a2c228ba31243bfd0578ce3abc2fc3758aa15f
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-11-13 15:45:02 -0400

    Creating first `footer`

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,43 @@
+import {
+  Envelope,
+  GithubLogo,
+  LinkedinLogo,
+  WhatsappLogo
+} from '@phosphor-icons/react'
+
 export function Footer() {
   return (
-    <div>
-      <a
-          href="br.qr-code-generator.com/"
-          border="0"
-          // style="cursor:default"
-          rel="nofollow"
+    <footer className="lg:max-w-5xl m-auto">
+      <div className="flex justify-center gap-4 py-4">
+        <a
+          href="https://www.linkedin.com/in/enickjhony/"
+          target="_blank"
+          rel="noreferrer"
         >
-          <img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fenick.dev.br%2F&chs=180x180&choe=UTF-8&chld=L|2" />
+          <LinkedinLogo size={32} color="#0A66C2" />
         </a>
-    </div>
+        <a
+          href="https://github.com/EnickJhony"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GithubLogo size={32} />
+        </a>
+        <a href="mailto:enick.jhony@gmail.com" target="_blank" rel="noreferrer">
+          <Envelope size={32} />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send?phone=5592981342681&amp;text=Hello, more information!"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <WhatsappLogo size={32} color="#16a34a" />
+        </a>
+      </div>
+      <div className="text-sm text-slate-500 text-center underline">
+        <p>Desenvolvido por EnickJhony</p>
+        <p>&copy; 2023</p>
+      </div>
+    </footer>
   )
 }
