1b02c096ff391236682272be2b19d18fc164ec5f
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-11-13 15:06:12 -0400

    modifying `deploy` and `repository` references

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,17 +7,21 @@ import ToDoListImg from '../assets/ToDoList.png'
 export function Projects() {
   return (
     <div className="lg:max-w-5xl m-auto">
-      <div className="text-3xl font-bold underline mb-8">
-        Projects
-      </div>
+      <div className="text-3xl font-bold underline mb-8">Projects</div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         <div>
           <div className="hover:scale-105 transform transition-all duration-500">
-            <img
-              className="w-full rounded-lg object-cover"
-              src={ToDoListImg}
-              alt=""
-            />
+            <a
+              href="https://todo-enickjhony.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="w-full rounded-lg object-cover"
+                src={ToDoListImg}
+                alt=""
+              />
+            </a>
           </div>
           <div className="flex justify-center items-center">
             <a title="Informações do projeto" href="#">
@@ -36,11 +40,17 @@ export function Projects() {
         </div>
         <div>
           <div className="hover:scale-105 transform transition-all duration-500">
-            <img
-              className="w-full rounded-lg object-cover"
-              src={CoffeeDeliveryImg}
-              alt=""
-            />
+            <a
+              href="https://coffee-delivery-enickjhony.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="w-full rounded-lg object-cover"
+                src={CoffeeDeliveryImg}
+                alt=""
+              />
+            </a>
           </div>
           <div className="flex justify-center items-center">
             <a title="Informações do projeto" href="#">
@@ -49,7 +59,7 @@ export function Projects() {
             <a
               className="p-4"
               title="Repositório do projeto"
-              href="https://github.com/EnickJhony/todo"
+              href="https://github.com/EnickJhony/coffee-delivery"
               target="_blank"
               rel="noreferrer"
             >
@@ -59,11 +69,17 @@ export function Projects() {
         </div>
         <div>
           <div className="hover:scale-105 transform transition-all duration-500">
-            <img
-              className="w-full rounded-lg object-cover"
-              src={DoctorCareImg}
-              alt=""
-            />
+            <a
+              href="https://enickjhony.github.io/NWL-Origin-2022/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="w-full rounded-lg object-cover"
+                src={DoctorCareImg}
+                alt=""
+              />
+            </a>
           </div>
           <div className="flex justify-center items-center">
             <a title="Informações do projeto" href="#">
@@ -72,7 +88,7 @@ export function Projects() {
             <a
               className="p-4"
               title="Repositório do projeto"
-              href="https://github.com/EnickJhony/todo"
+              href="https://github.com/EnickJhony/NWL-Origin-2022"
               target="_blank"
               rel="noreferrer"
             >
@@ -82,11 +98,17 @@ export function Projects() {
         </div>
         <div>
           <div className="hover:scale-105 transform transition-all duration-500">
-            <img
-              className="w-full rounded-lg object-cover"
-              src={AnselImg}
-              alt=""
-            />
+            <a
+              href="https://enickjhony.github.io/portfolio-ansel/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="w-full rounded-lg object-cover"
+                src={AnselImg}
+                alt=""
+              />
+            </a>
           </div>
           <div className="flex justify-center items-center">
             <a title="Informações do projeto" href="#">
@@ -95,7 +117,7 @@ export function Projects() {
             <a
               className="p-4"
               title="Repositório do projeto"
-              href="https://github.com/EnickJhony/todo"
+              href="https://github.com/EnickJhony/portfolio-ansel"
               target="_blank"
               rel="noreferrer"
             >
