c2fdae9bea64427bb711e95186bb3a47f8ff0343
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-11-14 15:45:19 -0400

    ✨ feat: adding vercel `Analytics`

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Hero } from './components/Hero'
 import { LoadingPage } from './components/LoadingPage'
 import { Navigation } from './components/Navigation'
 import { Projects } from './components/Projects'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true)
@@ -24,6 +25,7 @@ export default function App() {
       <Hero />
       <Projects />
       <Footer />
+      <Analytics />
     </div>
   )
 }
