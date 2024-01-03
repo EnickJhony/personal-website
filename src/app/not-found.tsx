62271aabd6e965f0b5cf5861a86f529ff4bf50bd
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-03 14:15:09 -0400

    creating `not found` page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <main className="min-h-screen flex justify-center items-center bg-slate-950 text-slate-300">
+      <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold p-4">Vish, eu não fiz essa página...</h1>
+    </main>
+  )
+}
