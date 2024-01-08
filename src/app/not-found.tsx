6015713b8e39146567cdf52db4e2763200d038ba
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-08 16:31:10 -0400

    🔧 refactor: Adding page size control

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,9 @@
 export default function Custom404() {
   return (
-    <main className="min-h-screen flex justify-center items-center bg-slate-950 text-slate-300">
-      <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold p-4">Vish, eu não fiz essa página...</h1>
+    <main className="lg:max-w-5xl mx-auto p-4 flex flex-col justify-center items-center h-96">
+      <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold">
+        Vish, eu não fiz essa página...
+      </h1>
     </main>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 export function Navigation() {
   return (
-    <nav className="flex justify-between items-center py-4">
+    <nav className="lg:max-w-5xl mx-auto p-4 flex justify-between items-center">
       <div>
         <span className="font-bold text-5xl">e³</span>
       </div>
