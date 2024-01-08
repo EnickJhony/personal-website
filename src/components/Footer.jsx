fa9d478e21158689f0b54a7fbb4e36f3c3b15df9
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-08 16:32:49 -0400

    🔧 refactor: Modifying the project `footer`

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,36 +7,41 @@ import {
 
 export function Footer() {
   return (
-    <footer className="">
-      <div className="flex justify-center gap-4 py-4">
-        <a
-          href="https://www.linkedin.com/in/enickjhony/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <LinkedinLogo size={32} color="#0A66C2" />
-        </a>
-        <a
-          href="https://github.com/EnickJhony"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <GithubLogo size={32} />
-        </a>
-        <a href="mailto:enick.jhony@gmail.com" target="_blank" rel="noreferrer">
-          <Envelope size={32} />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send?phone=5592981342681&amp;text=Hello, more information!"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <WhatsappLogo size={32} color="#16a34a" />
-        </a>
-      </div>
-      <div className="text-sm text-slate-500 text-center underline">
-        <p>Desenvolvido por EnickJhony</p>
-        <p>&copy; 2023</p>
+    <footer>
+      <div className="lg:max-w-5xl mx-auto p-8 flex flex-col gap-4">
+        <div className="flex justify-center gap-4">
+          <a
+            href="https://www.linkedin.com/in/enickjhony/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedinLogo size={32} color="#0A66C2" />
+          </a>
+          <a
+            href="https://github.com/EnickJhony"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GithubLogo size={32} />
+          </a>
+          <a
+            href="mailto:enick.jhony@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Envelope size={32} />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=5592981342681&amp;text=Hello, more information!"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <WhatsappLogo size={32} color="#16a34a" />
+          </a>
+        </div>
+        <div className="text-base text-slate-500 text-center">
+          <p>&copy; 2024 enick.dev.br</p>
+        </div>
       </div>
     </footer>
   )
