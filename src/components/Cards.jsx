98e584430cb3998c01acade3389512d9f7b34af9
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-08 18:09:57 -0400

    🔧 refactor: Adjusting `cards` and `navigation` links

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,7 +11,7 @@ export function Cards() {
     <div className="lg:max-w-5xl mx-auto p-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         <div>
-          <Link href="#" target="_blank" rel="noreferrer">
+          <Link href={"/about"}>
             <div className="hover:scale-105 transform transition-all duration-500 bg-slate-900 rounded-lg h-40 md:h-44 lg:h-36 flex justify-center items-center text-2xl">
               <p className="pl-4">Sobre mim &nbsp;</p>
               <p className="pr-4">
@@ -21,17 +21,17 @@ export function Cards() {
           </Link>
         </div>
         <div>
-          <Link href="#" target="_blank" rel="noreferrer">
+          <Link href={"/projects"}>
             <div className="hover:scale-105 transform transition-all duration-500 bg-slate-900 rounded-lg h-40 md:h-44 lg:h-36 flex justify-center items-center text-2xl">
-              <p className="pl-4">Meus projetos &nbsp;</p>
-              <p className="pr-4">
+              <p className="pl-3">Meus projetos &nbsp;</p>
+              <p className="pr-3">
                 <ProjectorScreen size={36} />
               </p>
             </div>
           </Link>
         </div>
         <div>
-          <Link href="#" target="_blank" rel="noreferrer">
+          <Link href={"/work"}>
             <div className="hover:scale-105 transform transition-all duration-500 bg-slate-900 rounded-lg h-40 md:h-44 lg:h-36 flex justify-center items-center text-2xl">
               <p className="pl-4">Educação / Trabalho &nbsp;</p>
               <p className="pr-4">
@@ -41,7 +41,7 @@ export function Cards() {
           </Link>
         </div>
         <div>
-          <Link href="#" target="_blank" rel="noreferrer">
+          <Link href={"/hobbies"}>
             <div className="hover:scale-105 transform transition-all duration-500 bg-slate-900 rounded-lg h-40 md:h-44 lg:h-36 flex justify-center items-center text-2xl">
               <p className="pl-4">Hobbies &nbsp; </p>
               <p className="pr-4">
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link"
+
 export function Navigation() {
   return (
     <nav className="lg:max-w-5xl mx-auto p-4 flex justify-between items-center">
-      <div>
+      <Link href={"/"}>
         <span className="font-bold text-5xl">e³</span>
-      </div>
+      </Link>
       <span></span>
     </nav>
   )
