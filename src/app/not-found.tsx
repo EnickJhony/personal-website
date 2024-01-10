2266ce282eded663c975c973170f4046b9d78843
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-10 18:36:13 -0400

    🔧 refactor: Adjusting `UnderConstruction` and `NotFound` page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link"
+
 export default function Custom404() {
   return (
-    <main className="lg:max-w-5xl mx-auto p-4 flex flex-col justify-center items-center h-96">
+    <main className="lg:max-w-5xl mx-auto p-4 flex flex-col justify-center items-center h-[500px]">
       <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold">
         Vish, eu não fiz essa página...
       </h1>
+      <p className="text-xl underline m-8">
+        <Link href={"/"}>Voltar pra home</Link>
+      </p>
     </main>
   )
 }
--- src/app/underConstruction/page.tsx
+++ src/app/underConstruction/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 export default function UnderConstruction() {
   return (
-    <div>
-      <h1>Ainda estou trabalhando nessa página</h1>
-    </div>
+    <main className="lg:max-w-5xl mx-auto p-4 flex flex-col justify-center items-center h-[500px]">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+        Poxa, ainda não terminei essa página...
+      </h1>
+      <p className="text-xl underline m-8">
+        <Link href={"/"}>Voltar pra home</Link>
+      </p>
+    </main>
   )
 }
